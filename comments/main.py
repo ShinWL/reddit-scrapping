fd1fb993f80016151c77e042bf398bf25d0d3eae
ShinWL <shinwei@weeclicks.com.my>
2018-10-12 18:58:02 +0800

    updates 658

--- comments/main.py
+++ comments/main.py
@@ -1,7 +1,84 @@
 import requests
 from bs4 import BeautifulSoup
-from comments.DataOperation import SubReddit
-from comments.models import Post, Comment
+from models import Post, Comment
+
+def store_comments(post_url, comments):
+		# Comment.objects.all().delete()
+		# Delete post if no matched comments
+		if len(comments) == 0:
+			post_to_be_deleted = Post.objects.filter(post_url=post_url)
+			for post in post_to_be_deleted:
+				post.delete()
+			return
+		# Else save comments
+		def save_comments(post, user_name, comment_content):
+			comment = Comment.objects.create(
+					post=post,
+					user_name=user_name,
+					comment_content=comment_content
+				)
+			comment.save()
+
+		for comment_data in comments:
+			post_filtered = Post.objects.filter(post_url=post_url)
+			if not post_filtered:
+				post_filtered = Post.objects.create(
+					post_title=comment_data['post_title'],
+					post_url=post_url
+					)
+				post_filtered.save()
+				save_comments(post_filtered, comment_data['user_name'], comment_data['comment_content'])
+				continue
+			for post_data in post_filtered:
+				post_data.post_content = comment_data['post_content']
+				post_data.save()
+				save_comments(post_data, comment_data['user_name'], comment_data['comment_content'])
+
+def get_comments(post_url, more_comment, comments):
+		headers = {'User-Agent': 'Mozilla/5.0'}
+		r = requests.get(post_url + '.json', headers=headers)
+		comment_threads = r.json()[1]['data']['children']
+		post_title = r.json()[0]['data']['children'][0]['data']['title']
+		post_content = r.json()[0]['data']['children'][0]['data']['url']
+		# print(post_title)
+		if len(comment_threads) == 0:
+			return
+		last_thread = comment_threads[len(comment_threads) - 1]
+		if last_thread['kind'] == 'more':
+			list_id = last_thread['data']['children']
+			comment_threads.pop()
+			for comment_id in list_id:
+				more_comment.append(post_url+comment_id+'/')
+		
+		def recursively_get_replies(data, post_title, post_content):
+			# self.counter += 1
+			# print('comment counter:',self.counter)
+			if re.search(r'[\w\.-]+@[\w\.-]+', data['body']):
+				comments.append(
+					{
+					'post_title': post_title,
+					'post_content': post_content,
+					'user_name' : data['author'],
+					'comment_content': re.findall(r'[\w\.-]+@[\w\.-]+', data['body'])
+					})
+			if data['replies'] == '':
+				return
+			replies = data['replies']['data']['children']
+			for reply in replies:
+				if reply['kind'] == 'more':
+					list_id = reply['data']['children']
+					for comment_id in list_id:
+						more_comment.append(post_url+comment_id+'/')
+					return
+				recursively_get_replies(reply['data'], post_title, post_content)
+
+		for thread in comment_threads:
+			data = thread['data']
+			recursively_get_replies(data, post_title, post_content)
+
+def get_more_comments(more_comment):
+	for comment_url in more_comment:
+		get_comments(comment_url, more_comment)
 
 def store_posts(posts_data):
 	# Post.objects.all().delete()
@@ -46,8 +123,8 @@ def get_subreddits(reddit):
 			## start processing
 			for sr in subreddits:
 				post = []
+				bs = get_current_page_HTML(sr)
 				while True:
-					bs = get_current_page_HTML(sr)
 					for post in bs.find_all('div', class_='thing'):
 						post_title = post.find('p', class_="title").text
 						post_url = 'https://www.reddit.com' + (post.get('data-permalink'))
@@ -57,6 +134,12 @@ def get_subreddits(reddit):
 						})
 					store_posts(posts)
 					### STORE COMMENT ###
+					more_comment = []
+					comments = []
+					for post in posts:
+						get_comments(post['post_url'], more_comment, comments)
+						get_more_comments(more_comment)
+						store_comment(post['post_url'], comments)
 					next_button = bs.find("span", class_="next-button")
 					if not next_button:
 						break
